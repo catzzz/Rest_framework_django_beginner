69176aeb4afcb91957de4a6fcea28056f882173c
BioInspira <jimmyleu@bioinspira.com>
2020-04-24 14:02:22 +0800

    Added our first viewset and router.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -52,9 +54,14 @@ class HelloApiView(APIView):
         return Response({'method':'DELETE'})
 
 
-'''
-def patch(self,request, pk=None):
-    """Patch request, only updates fields provided in the request."""
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
 
-    return Response({'method':'patch'})
-'''
+    def list(self,request):
+        """Return a hello message."""
+        a_viewset =[
+            'Uses actions(list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+        return Response({'message':'Hello!','a_viewset':a_viewset})
